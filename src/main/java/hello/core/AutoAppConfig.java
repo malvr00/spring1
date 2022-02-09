e48741b93898af381598641e72f0b66d127d668a
조용일 <yedn1128@naver.com>
2022-02-10 00:09:19 +0900

    탐색 위치와 기본 스캔 대상

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -6,6 +6,9 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        // basePackages는 스캔을 시작할 위치를 지정, 여러개도 지정 가능.
+        // 지정하지 않으면 ComponentScan이 붙은 설정 정보 클래스의 패키지가 시작 위치가 된다
+//        basePackages = "hello.core.member",
         // 모드 끌어올린 Bean중에서 뺄거 지정
         // Configuration도 Bean에 등록되기 때문에 충돌이 발생 할 수 있다.
         // 그래서 Configuration은 뺀다.
@@ -14,3 +17,4 @@ import org.springframework.context.annotation.FilterType;
 )
 public class AutoAppConfig {
 }
+
