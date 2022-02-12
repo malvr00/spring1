15bfad63dd9f0b80d7f1d412c9e0e65756277af7
조용일 <yedn1128@naver.com>
2022-02-12 16:05:13 +0900

    애노테이션 직접 만들기

--- src/main/java/hello/core/Order/OrderServiceImpl.java
+++ src/main/java/hello/core/Order/OrderServiceImpl.java
@@ -1,5 +1,6 @@
 package hello.core.Order;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
@@ -25,7 +26,7 @@ public class OrderServiceImpl implements OrderService{
 //
 //    }
 
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
 
