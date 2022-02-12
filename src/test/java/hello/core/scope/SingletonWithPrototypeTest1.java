24147da7d3d2c58845074c3d48a9395b488784c9
조용일 <yedn1128@naver.com>
2022-02-13 02:17:28 +0900

    싱글톤 빈과 함께 사용시 Provider로 문제 해결

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -2,11 +2,13 @@ package hello.core.scope;
 
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -35,15 +37,24 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean bean2 = ac.getBean(ClientBean.class);
         int count2 = bean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
     }
 
     @Scope("singleton")
     @RequiredArgsConstructor
     static class ClientBean{
-        private final PrototypeBean prototypeBean;
+//        private final PrototypeBean prototypeBean;
+
+        // 지정한 빈을 컨테이너에서 대신 찾아주는 DL(디펜더시 룩업) 서비스를 제공하는 기능
+//        private final ObjectProvider<PrototypeBean> prototypeBeanProvider;
+
+        // 자바 표준 기술이다. 하지만 라이브러리를 추가해서 사용해야함.
+        private final Provider<PrototypeBean> prototypeBeanProvider;
 
         public int logic(){
+            // 항상 새로운 프로토타입 빈이 생성된다. ( 대신 조회해주는 대리자 )
+//            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();
             prototypeBean.addCount();
             return prototypeBean.getCount();
         }
